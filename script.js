2c0e888fd87f979a1e88ec7fa1e876c17001be16
valerie <valerie_eng@berkeley.edu>
2025-03-18 17:33:27 -0700

    change infrastructure

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 80,
+            behavior: 'smooth'
+        });
+    });
+});
